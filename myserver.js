b5fded933133ce26620b816f18e19fe6fa37aaf9
Natan Cabral <natancabral@gnial.com.br>
2021-06-29 17:31:24 -0300

    Update myserver.js

--- myserver.js
+++ myserver.js
@@ -5,7 +5,7 @@ const port = 3311;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+  res.end('Hello World');
 }).listen(port, hostname, () => {
-  console.log('Server running at http://${hostname}:${port}/');
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
